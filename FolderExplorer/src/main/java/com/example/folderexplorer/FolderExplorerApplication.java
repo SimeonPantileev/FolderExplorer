4f617afccca7900936e8731042a75e892f270631
simeon <simeon@telerikacademy.com>
2024-04-05 10:48:03 +0300

    init project

---
+++ FolderExplorer/src/main/java/com/example/folderexplorer/FolderExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.example.folderexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolderExplorerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FolderExplorerApplication.class, args);
+    }
+
+}
---
+++ FolderExplorer/src/test/java/com/example/folderexplorer/FolderExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.folderexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolderExplorerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
